031450ff4e2617fbd927633e047861cdde8e6b81
shekhawatproperties <shekhawatproperties1@gmail.com>
2025-09-08 21:29:43 +0530

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
 // firebase-config.js
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyBL1vV62gUr5_yoMJmNUGegIu4_37F_-bk",
+  authDomain: "shekhawat-market.firebaseapp.com",
+  projectId: "shekhawat-market",
+  storageBucket: "shekhawat-market.firebasestorage.app",
+  messagingSenderId: "982626645386",
+  appId: "1:982626645386:web:28bab34175197b06af8473",
+  measurementId: "G-F87FPLNXZJ"
 };
 
 // --- DO NOT EDIT BELOW THIS LINE ---
@@ -17,9 +18,21 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-s
 import { getFunctions } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-functions.js";
 
 const app = initializeApp(firebaseConfig);
+
+import { initializeAppCheck, ReCaptchaV3Provider } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app-check.js";
+
+// ⚠️ reCAPTCHA v3 site key 
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider("6LfhbcIrAAAAAA0FIhRoGcNfVU6_Nk1IqeW-_kzc"),
+  isTokenAutoRefreshEnabled: true // recommended
+});
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const functions = getFunctions(app);
 
 export { db, auth, storage, functions };
+
+
+
